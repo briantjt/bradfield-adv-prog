a8ab5b566bab47b96ef083bc17072fb70b1f8f23
Brian Teh <tjtbrian@gmail.com>
2021-11-08 10:54:59 +0800

    fix memory leak

--- cgo/rocks_go/rocksdb/rocksdb.go
+++ cgo/rocks_go/rocksdb/rocksdb.go
@@ -2,16 +2,21 @@ package rocksdb
 
 /*
 #cgo CFLAGS: -I/usr/include
-// #cgo LDFLAGS: -L/usr/lib ${SRCDIR}/librocksdb.a -lstdc++ -lpthread -lpthread -lsnappy  -lz -lbz2 -lzstd -llz4 -ldl -luring -lm
-#cgo LDFLAGS: -L/usr/lib -lrocksdb
+#cgo LDFLAGS: -L/usr/lib ${SRCDIR}/librocksdb.a -lstdc++ -lpthread -lpthread -lsnappy  -lz -lbz2 -lzstd -llz4 -ldl -luring -lm
+// #cgo LDFLAGS: -L/usr/lib -lrocksdb
+#include <stdlib.h>
 #include <string.h>
 #include "rocksdb/c.h"
 #include "rockswrapper.h"
 */
 import "C"
-import "errors"
+import (
+	"errors"
+	"unsafe"
+)
 
 type rocksdb struct {
+	path         *C.char
 	db           *C.rocksdb_t
 	options      *C.rocksdb_options_t
 	writeOptions *C.rocksdb_writeoptions_t
@@ -20,17 +25,21 @@ type rocksdb struct {
 
 func NewRocksDB(path string) *rocksdb {
 	var rocksDB rocksdb
-	C.initDB(C.CString(path), &rocksDB.db, &rocksDB.options, &rocksDB.writeOptions, &rocksDB.readOptions)
+	rocksDB.path = C.CString(path)
+	C.initDB(rocksDB.path, &rocksDB.db, &rocksDB.options, &rocksDB.writeOptions, &rocksDB.readOptions)
 	return &rocksDB
 }
 
 func (rdb *rocksdb) Close() {
 	C.closeDB(rdb.db, rdb.options, rdb.writeOptions, rdb.readOptions)
+	C.free(unsafe.Pointer(rdb.path))
 }
 
 func (rdb *rocksdb) Put(key, value string) error {
 	c_key := C.CString(key)
 	c_val := C.CString(value)
+	defer C.free(unsafe.Pointer(c_key))
+	defer C.free(unsafe.Pointer(c_val))
 	var err *C.char
 	C.rocksdb_put(rdb.db, rdb.writeOptions, c_key, C.strlen(c_key), c_val, C.strlen(c_val),
 		&err)
@@ -42,9 +51,11 @@ func (rdb *rocksdb) Put(key, value string) error {
 
 func (rdb *rocksdb) Get(key string) (string, error) {
 	c_key := C.CString(key)
+	defer C.free(unsafe.Pointer(c_key))
 	var err *C.char
 	var len C.size_t
 	returned_value := C.rocksdb_get(rdb.db, rdb.readOptions, c_key, C.strlen(c_key), &len, &err)
+	defer C.free(unsafe.Pointer(returned_value))
 	if err != nil {
 		return "", errors.New(C.GoString(err))
 	}
